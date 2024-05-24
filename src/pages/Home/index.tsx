266bdd82c784d2eef545ca6b866e5a683c20ce99
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-05-24 20:07:57 -0300

    Reset do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,12 +36,17 @@ export function Home() {
 
   const { handleSubmit, watch, reset } = newCycleForm;
 
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data);
+    reset();
+  }
+
   const task = watch('task');
   const isSubmitDisable = !task;
 
   return (
     <HomeContainer>
-      <form action="" onSubmit={handleSubmit(createNewCycle)}>
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
